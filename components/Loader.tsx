67343d3172901b7ac1506e0dc1c1d4d121f8ceac
ATechproC <choraichianass@gmail.com>
2025-09-14 22:17:09 +0200

    update

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image'
 
 const Loader = () => {
     return (
-        <div className="flex-items flex-col gap-2 text-white absolute top-[50%] left-[50%] -translate-x-|50%] -translate-y-[50%]">
-            <Image
+        // <div className="flex-items flex-col gap-2 text-white absolute top-[50%] left-[50%] -translate-x-|50%] -translate-y-[50%]"
+        <div className='absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+            <div className='flex-items flex-col gap-2 text-white '>
+                <Image
                 src={assets.loader}
                 alt="loader"
                 width={32}
@@ -12,6 +14,7 @@ const Loader = () => {
                 className="animate-spin"
             />
             Loading...
+            </div>
         </div>
     )
 }
